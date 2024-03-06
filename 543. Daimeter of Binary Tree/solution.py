b9b0b65772985f4ef79b2ed551e4becfcf3875a5
Pk_Patel <parthpatel9414@gmail.com>
2024-03-05 21:14:34 -0500

    Add solution for calculating the diameter of a binary tree

---
+++ 543. Daimeter of Binary Tree/solution.py
@@ -0,0 +1,26 @@
+from typing import *
+
+class TreeNode:
+    def __init__ (self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def daimeterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        result = 0
+
+        def dfs(root):
+            nonlocal result
+
+            if not root:
+                return 0
+
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            result = max(result, left + right)
+
+            return 1 + max(left + right)
+        dfs(root)
+        return result
